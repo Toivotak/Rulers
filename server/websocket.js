5642d7570c88b646e60b5d498a18c7247a060583
Toivotak <50209071+Toivotak@users.noreply.github.com>
2021-04-25 18:38:18 +0300

    cleaning

--- server/websocket.js
+++ server/websocket.js
@@ -1,41 +1,22 @@
-/*
-    Modify this implementation to send a random number on range 20-180 
-    to connected clients at one second intervals
-*/
 
-
-module.exports=function(port){
+module.exports = function(port){
 	let ws = require("nodejs-websocket")
-    let connections=[];
-    let prev=100;
-
-    function sendNew(){
-		let next=Math.round(prev+Math.random()*20-10);
-		if (next>180) next=180;
-		if (next<20) next=20;
-        connections.forEach(c => {
-            c.sendText(String(next));
-        })
-        prev=next;
-        setTimeout(sendNew,1000);
-    }
-
-    setTimeout(sendNew,1000);
+    let connections = [];
 
 	ws.createServer(function(conn) {
 		connections.push(conn);
 		console.log("New websocket connection");
 		conn.on("text", function (str) {
-			console.log("Received "+str)
+			console.log("Received " + str)
 		});
 		conn.on("close", function (code, reason) {
 			console.log("Closing websocket");
 		})
 		conn.on("error",function(err){
-            let index=connections.indexOf(conn);
-            connections.splice(index,1);
+            let index = connections.indexOf(conn);
+            connections.splice(index, 1);
 			console.log("Websocket error");
 		});
 	}).listen(port);
-	console.log("WebSocket server listening port "+port);
+	console.log("WebSocket server listening port " + port);
 }
